ecc7cea0ce0b3f6178e4a5d183416cf8be6786fd
Anastassia Davis <demivisage@gmail.com>
2019-02-16 04:04:21 +0000

    Rename files; add ex. 4

---
+++ 001_basics/001_04.rb
@@ -0,0 +1,10 @@
+movies = {
+  "Avengers: Infinity War" => 2018,
+  "Thor: Ragnarok" => 2018,
+  "Iron Man" => 2008,
+  "Guardians of the Galaxy" => 2014,
+  "Captain America: Civil War" => 2016}
+  
+movies.each do |film, year| 
+  puts "#{year}"
+end
