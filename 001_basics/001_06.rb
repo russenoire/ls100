13bdfdac8bcbde1a5e303f1b15f380c5138021ac
Anastassia Davis <demivisage@gmail.com>
2019-02-16 05:17:19 +0000

    Chain .chomp to gets for string input, oops

--- 001_basics/001_06.rb
+++ 001_basics/001_06.rb
@@ -1,10 +1,10 @@
 puts "Enter 3 floating-point numbers, please.\nI'll square them for you."
 print "Your first number? "
-num1 = gets.to_f
+num1 = gets.chomp.to_f
 print "Your second number? "
-num2 = gets.to_f
+num2 = gets.chomp.to_f
 print "And your last number, please. "
-num3 = gets.to_f
+num3 = gets.chomp.to_f
 
 puts "Your first square: " + (num1 ** 2).to_s
 puts "Your second square: " + (num2 ** 2).to_s
