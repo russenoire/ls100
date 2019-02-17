aa96619d8775cf83fc107a178ba2d30f0d6da904
Anastassia Davis <demivisage@gmail.com>
2019-02-17 02:30:16 +0000

    Add 4th ex, 2nd chap

--- 002_variables/name.rb
+++ 002_variables/name.rb
@@ -1,11 +1,19 @@
-print "What's your name?"
-name = gets.chomp
+# exercise 4
+print "What's your first name? "
+first_name = gets.chomp.capitalize!
+print "What's your last name? "
+last_name = gets.chomp.capitalize!
+puts "#{first_name} #{last_name}"
 
 # exercise 1
-puts "Hi #{name}! How's it going?"
+# puts "Hi #{name}! How's it going?"
 
 # exercise 3
-10.times do
-  puts "#{name}"
-end
+# 10.times do
+#  puts "#{name}"
+# end
+
+
+
+
 
