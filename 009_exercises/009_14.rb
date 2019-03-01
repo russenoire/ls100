135e0052fea8dc184f42cbc7b55fa06be411bce9
Anastassia Davis <demivisage@gmail.com>
2019-03-01 05:15:53 +0000

    Add better soln for ex. 14

--- 009_exercises/009_14.rb
+++ 009_exercises/009_14.rb
@@ -3,8 +3,16 @@ contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
 contact_data_labels = [:email, :address, :phone]
 
+=begin
 contact_data_labels.each do |e|
   contacts["Joe Smith"][e] = contact_data[0].shift
   contacts["Sally Johnson"][e] = contact_data[1].shift
-end
+=end
 
+# refactor
+contact_data.flatten!
+contact_data_labels.each do |label|
+  contacts.each do |name, hash| 
+    contacts[name][label] = contact_data.shift
+  end
+end
