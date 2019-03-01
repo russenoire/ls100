9c7eb75370dd559a321807f361a621d2441ec10e
Anastassia Davis <demivisage@gmail.com>
2019-03-01 04:16:05 +0000

    Make contact_data_labels in ex. 12 array of symbols

--- 009_exercises/009_13.rb
+++ 009_exercises/009_13.rb
@@ -1,7 +1,7 @@
 contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
             ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
 contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
-contact_data_labels = [:email, :address, :phone]
+contact_data_labels = ['email', 'address', 'phone']
 
 contacts["Joe Smith"] = {
   contact_data_labels[0] => contact_data[0][0],
@@ -15,6 +15,6 @@ contacts["Sally Johnson"] = {
   contact_data_labels[2] => contact_data[1][2]
 }
 
-puts contacts["Joe Smith"]
-puts contacts["Sally Johnson"]
+puts contacts["Joe Smith"][:email]
+puts contacts["Sally Johnson"][:phone]
 
