ed3af1d3c28715d84345071cdcceaac5d71648bf
Anastassia Davis <demivisage@gmail.com>
2019-03-01 04:10:51 +0000

    Add ex. 10 and 12

---
+++ 009_exercises/009_10.rb
@@ -0,0 +1,17 @@
+# hash values can be arrays, and arrays can contain hashes.
+
+# hash containing arrays:
+grains = {
+  wheat: ['spelt','triticale','farro','bulgur','durum']
+  oats: 'scots oats'
+  barley: 'pearl barley'
+  rapeseed: ['canola','rapeseed']
+}
+
+# array containing hashes:
+names = [
+  {greek: ['genesis','aristotle','athena']}
+  {arabic: ['tariq','aisha','sharbat']}
+]
+
+
---
+++ 009_exercises/009_12.rb
@@ -0,0 +1,20 @@
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+contact_data_labels = ['email', 'address', 'phone']
+
+contacts["Joe Smith"] = {
+  contact_data_labels[0] => contact_data[0][0],
+  contact_data_labels[1] => contact_data[0][1],
+  contact_data_labels[2] => contact_data[0][2]
+}
+
+contacts["Sally Johnson"] = {
+  contact_data_labels[0] => contact_data[1][0],
+  contact_data_labels[1] => contact_data[1][1],
+  contact_data_labels[2] => contact_data[1][2]
+}
+
+puts contacts["Joe Smith"]
+puts contacts["Sally Johnson"]
+
