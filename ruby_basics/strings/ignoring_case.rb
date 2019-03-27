89108a643b77f74f8ac547bef7f09d7c93374824
Anastassia Davis <demivisage@gmail.com>
2019-03-27 03:18:03 +0000

    Add more Ruby basics exercises

--- ruby_basics/strings/ignoring_case.rb
+++ ruby_basics/strings/ignoring_case.rb
@@ -1,6 +1,8 @@
 name = %Q(Roger)
+# initial solution
 puts name.downcase == %Q(RoGer).downcase 
 puts name.downcase == %Q(DAVE).downcase
 
+# after looking at published solution
 puts name.casecmp('RoGer') == 0
 puts name.casecmp('DAVE') == 0
